a0c1becc5f57b61beb0b5a835c1036db1f7a8ce9
KAIVIOR <wupeter99989@gmail.com>
2021-07-02 18:27:55 +0800

    Initial Commit

---
+++ YearChanllenge/YearChanllengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YearChanllengeApp.swift
+//  YearChanllenge
+//
+//  Created by 吳庭愷 on 2021/7/2.
+//
+
+import SwiftUI
+
+@main
+struct YearChanllengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
